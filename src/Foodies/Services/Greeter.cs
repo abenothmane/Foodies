610b4bc253a6d565ac4af632a7ce03309ccbafba
Anas <num_ein2@hotmail.com>
2017-04-14 11:54:08 -0700

    Initial commit

---
+++ src/Foodies/Services/Greeter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodies.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+
+    public class Greeter : IGreeter 
+    {
+        private string _config;
+
+        public Greeter(IConfiguration config)
+        {
+            _config = config["greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _config;
+        }
+    }
+}
